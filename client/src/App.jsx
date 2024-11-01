14e82586e84319bff03e682fcfe1122ac5167cb7
ajojose803 <ajojose803@gmail.com>
2024-11-01 16:20:10 +0530

    Added Pages Structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUIp from "./pages/SignUIp";
+import PrivateRoute from "./components/PrivateRoute";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUIp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ client/src/pages/SignUIp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignUIp() {
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+        <h1 className="text-3xl text-center font-semibold my-7"> Sign Up</h1>
+    </div>
+  )
+}
+
+export default SignUIp  
