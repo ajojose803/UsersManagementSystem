08269051a7b3483ce9b41d1afc2cd186023640a8
ajojose803 <ajojose803@gmail.com>
2024-11-01 23:18:54 +0530

    Error Handling Middleware setup

--- api/index.js
+++ api/index.js
@@ -1,24 +1,34 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js'
-import authRoutes from './routes/auth.route.js'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
-
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-    console.log('Connected to MongoDB');
-})
-.catch((error) => {
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
     console.log(error);
-})
+  });
 
 const app = express();
 
-app.listen(3000, ()=> {
-    console.log('Server Listening on port 3000');
-})
+app.listen(3000, () => {
+  console.log("Server Listening on port 3000");
+});
 
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
-app.use('/api/user', userRoutes )
-app.use('/api/auth', authRoutes);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
