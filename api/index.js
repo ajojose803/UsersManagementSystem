693c974caa4ae572dd38d6a1c1a86d8720c3c57e
ajojose803 <ajojose803@gmail.com>
2024-11-12 10:16:39 +0530

    Added CookieParser

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 mongoose
@@ -16,12 +17,16 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
+app.use(cookieParser());
+
 app.listen(3000, () => {
   console.log("Server Listening on port 3000");
 });
-app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
