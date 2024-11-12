49ba66db838f5bbb613c466cb13618fe5071a82b
ajojose803 <ajojose803@gmail.com>
2024-11-12 21:49:27 +0530

    Admin Side Login Routes Fixed

--- api/routes/admin.route.js
+++ api/routes/admin.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { adminLogin } from "../controllers/admin.controller";
+import { adminLogin } from "../controllers/admin.controller.js";
 
 const router = express.Router();
 router.post("/login", adminLogin);
